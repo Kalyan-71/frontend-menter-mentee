59d473ff13604a6a7bc02550d24152e75e500a2d
kalyan kumar <pothugantikalyankumar71@gmail.com>
2026-02-22 20:41:41 +0530

    urls for production

--- Backend routes/constant.js
+++ Backend routes/constant.js
@@ -1,16 +1,17 @@
-// /*
+/*
 const RegisterURL = "http://localhost:8000/api/v1/users/register";
 const LoginURL = "http://localhost:8000/api/v1/users/login";
 const LogOutURL = "http://localhost:8000/api/v1/users/logout";
-// */
+*/
 
 ///backend routes
-/*
+// /*
 
 const RegisterURL = "https://major-cudz.onrender.com/api/v1/users/register";
 const LoginURL = "https://major-cudz.onrender.com/api/v1/users/login"
+const LogOutURL = "https://major-cudz.onrender.com/api/v1/users/logout";
 
-*/
+// */
 export {
     RegisterURL, 
     LoginURL,
